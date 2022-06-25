ef2c459498d49c5e82e1d6c00e3563c624e33d73
Mario E <marioe@MacBook-Pro-de-Mario.local>
2022-06-25 16:29:12 -0500

    integrando animaciones y efectos

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
-import { ColorMatrix, Line, Canvas, Circle, Group, BackdropFilter, Paint, LinearGradient, vec, RoundedRect, usePaintRef, Blur } from "@shopify/react-native-skia";
+import { View, Button } from 'react-native';
+import {
+  ColorMatrix,
+  useClockValue,
+  useDerivedValue,
+  Canvas,
+  Circle,
+  Group,
+  BackdropFilter,
+  Paint,
+  LinearGradient,
+  vec,
+  RoundedRect,
+  usePaintRef,
+  Blur,
+  useSpring,
+  runSpring,
+  useValueEffect,
+  useValue,
+  BlurMask,
+  RadialGradient
+} from "@shopify/react-native-skia";
 
 const r = 64;
+const interval = 2500;
 
 const App = () => {
 
+  /* check useCanvas */
   const [state, setState] = useState({ width: 0, height: 0 });
   const paint = usePaintRef();
+
+  const clock = useClockValue();
+  const opacity = useDerivedValue(
+    () => {
+      return (clock.current % interval) / interval;
+    },
+    [clock]
+  );
+
+  const [toggled, setToggled] = useState(false);
+  const position = useSpring(toggled ? (state.height / 2) - (64 / 2) - 24 : 126, { velocity: 300, mass: 4, damping: 40 });
+
+  const loop = () => runSpring(position, 100);
 
   return (
     <View onLayout={event => setState(event.nativeEvent.layout)} style={{ backgroundColor: '#19162C', height: '100%', width: '100%' }} >
@@ -19,10 +54,11 @@ const App = () => {
             colors={["magenta", '#19162C']}
           />
         </Circle>
+
         <Paint ref={paint}>
           <ColorMatrix
             matrix={[
-              1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 8, -4,
+              1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 8, -6,
             ]}
           >
             <Blur blur={14} />
@@ -33,6 +69,7 @@ const App = () => {
           <Circle cx={(state.width / 2) + (r / 2)} cy={state.height / 2} r={r} color="magenta" />
           <Circle cx={(state.width / 2) - (r / 2)} cy={state.height / 2} r={r} color="yellow" />
         </Group>
+
         <Circle cx={state.width / 2} cy={state.height - (state.height / 4)} r={r}>
           <Paint opacity={0.7} color="#EC4899" >
             <LinearGradient
@@ -50,21 +87,44 @@ const App = () => {
             />
           </Paint>
         </Circle>
+
         <RoundedRect
-          x={(state.width / 2) - (800 / 2)}
-          y={(state.height / 2) - (64 / 2)}
-          width={800}
+          x={(state.width / 2) - (440 / 2)}
+          y={position}
+          width={440}
           height={64}
           r={25}
-          color="#19162C"
-          opacity={0.3}  
         >
+          <RoundedRect
+            x={(state.width / 2) - (440 / 2)}
+            y={position}
+            width={440}
+            height={64}
+            r={25}
+            color="#fff"
+            opacity={0.8}
+            style='stroke'
+          />
           <BackdropFilter
-            clip={{ x: (state.width / 2) - (800 / 2), y: (state.height / 2) - (64 / 2), width: 800, height: 64 }}
+            clip={{ x: (state.width / 2) - (800 / 2), y: (state.height / 2) - (64 / 2) - 24, width: 800, height: 64 }}
             filter={<Blur blur={20} />}
+          />
+          {/* <LinearGradient
+            start={vec((state.width), state.height - (state.height / 4))}
+            end={vec((state.width / 2) + r, state.height - (state.height / 4) + r)}
+            colors={["magenta", '#19162C']}
+          /> */}
+          <BlurMask blur={28} style="normal" />
+          <RadialGradient
+            colors={['#19162C']}
+            c={vec((state.width / 2), (state.height / 2))}
+            r={state.height * 1}
           />
         </RoundedRect>
       </Canvas>
+      <View style={{ marginBottom: 48 }} >
+        <Button title="Toggle" color={'white'} onPress={() => setToggled((p) => !p)/* loop() */} />
+      </View>
     </View>
   );
 };
